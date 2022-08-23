f9f2bd31617376ef4af84d1c3115f0e28a53154b
Jon Friend <62109917+jonnyfriendlychicago@users.noreply.github.com>
2022-08-23 17:13:19 -0500

    api boiler plate up and running

---
+++ src/main/java/com/jonfriend/playdatenow_v03b/PlaydatenowV03bApplication.java
@@ -0,0 +1,13 @@
+package com.jonfriend.playdatenow_v03b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaydatenowV03bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaydatenowV03bApplication.class, args);
+	}
+
+}
